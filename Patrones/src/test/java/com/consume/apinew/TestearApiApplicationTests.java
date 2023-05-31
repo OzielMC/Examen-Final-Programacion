472040890d35d75cf6c1359de27d5d2129e05907
OzielMC <133171291+OzielMC@users.noreply.github.com>
2023-05-30 22:01:28 -0600

    Ultimo commit

---
+++ Patrones/src/test/java/com/consume/apinew/TestearApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consume.apinew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestearApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
