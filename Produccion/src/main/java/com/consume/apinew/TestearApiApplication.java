272f64dea1b4acc89a766f152955ec0e4b2251cf
OzielMC <133171291+OzielMC@users.noreply.github.com>
2023-05-30 21:57:26 -0600

    Ultimo commit

---
+++ Produccion/src/main/java/com/consume/apinew/TestearApiApplication.java
@@ -0,0 +1,13 @@
+package com.consume.apinew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestearApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestearApiApplication.class, args);
+	}
+
+}
